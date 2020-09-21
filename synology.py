912ca97b6aedeb98b46e260f0db09ee5a14b38ce
Maximilian Eitelwein <maximilian@eitelwein.net>
2020-09-21 19:21:08 +0200

    config file setup

--- synology.py
+++ synology.py
@@ -1,2 +1,17 @@
 import configparser
 import os
+
+class Synology():
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config["internal"] = {}
+        self.config["internal"]["conf_file"] = "/etc/synologyUpdater"
+        self.config["internal"]["keys"]      = "['emailPort','emailUser','emailHost','emailSender','emailPassword',emailDestination','synologyPort','synologyIP','synologyUser','synologyPassowrd'"
+        
+        syncPath = os.getcwd() + "syng.json"
+        print(syncPath)
+    
+    
+    
+if __name__ == "__main__":
+    Synology()
